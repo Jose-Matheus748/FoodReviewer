73f09ebcdfefb32b37e90efd69751061c8230e72
Cauettt <cauetrodrigues@gmail.com>
2025-10-14 20:12:18 -0300

    Add UsuarioDTO

---
+++ backend/src/main/java/dto/UsuarioDTO.java
@@ -0,0 +1,41 @@
+package dto;
+
+public class UsuarioDTO {
+
+    private Long id;
+    private String apelido;
+    private String email;
+
+    public UsuarioDTO() {
+    }
+
+    public UsuarioDTO(Long id, String apelido, String email) {
+        this.id = id;
+        this.apelido = apelido;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
