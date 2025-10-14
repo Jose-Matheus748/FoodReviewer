9a524a7c36b8cb01f51780d1624fcc6bdb749c29
Cauettt <cauetrodrigues@gmail.com>
2025-10-14 19:17:24 -0300

    Agora vai essa bomba

---
+++ BackEnd/repositories/TabelaNutricionalRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import Entity.TabelaNutricional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TabelaNutricionalRepository extends JpaRepository<TabelaNutricional, Integer> {
+}
