a5d9c0b7bd42ac0c5107f6509d3ee2d54b81450b
Cauet <cauetrodrigues@gmail.com>
2025-10-28 22:22:31 -0300

    Versão funcional testada do backend

--- backend/src/main/java/com/foodreviewer/backend/controllers/IngredienteController.java
+++ backend/src/main/java/com/foodreviewer/backend/controllers/IngredienteController.java
@@ -1,4 +1,61 @@
 package com.foodreviewer.backend.controllers;
 
+import com.foodreviewer.backend.Entity.Ingrediente;
+import com.foodreviewer.backend.services.IngredienteService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/ingredientes")
 public class IngredienteController {
+
+    private final IngredienteService ingredienteService;
+
+    public IngredienteController(IngredienteService ingredienteService){
+        this.ingredienteService = ingredienteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingrediente> criarIngrediente( @RequestBody Ingrediente ingrediente){
+        return ResponseEntity.ok(ingredienteService.saveIngrediente(ingrediente));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ingrediente>> findAll(){
+        return ResponseEntity.ok(ingredienteService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Ingrediente>> findById(Long id){
+        try{
+            return ResponseEntity.ok(ingredienteService.findById(id));
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Optional<Ingrediente>> updateIngrediente(@PathVariable Long id,  @RequestBody Ingrediente ingrediente){
+        try{
+            return ResponseEntity.ok(ingredienteService.updateIngrediente(ingrediente, id));
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Optional<Ingrediente>> deleteIngrediente(@PathVariable Long id){
+        try{
+            ingredienteService.deleteIngrediente(id);
+            return ResponseEntity.noContent().build();
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- backend/src/main/java/com/foodreviewer/backend/controllers/TabelaNutricionalController.java
+++ backend/src/main/java/com/foodreviewer/backend/controllers/TabelaNutricionalController.java
@@ -1,4 +1,61 @@
 package com.foodreviewer.backend.controllers;
 
+import com.foodreviewer.backend.Entity.TabelaNutricional;
+import com.foodreviewer.backend.services.TabelaNutricionalService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tabelasNutricionais")
 public class TabelaNutricionalController {
+
+    private final TabelaNutricionalService tabelaNutricionalService;
+
+    public TabelaNutricionalController(TabelaNutricionalService tabelaNutricionalService){
+        this.tabelaNutricionalService = tabelaNutricionalService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TabelaNutricional> criarTabelaNutricional(@RequestBody TabelaNutricional tabelaNutricional){
+        return ResponseEntity.ok(tabelaNutricionalService.saveTabelaNutricional(tabelaNutricional));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TabelaNutricional>> findAll(){
+        return ResponseEntity.ok(tabelaNutricionalService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<TabelaNutricional>> findById(Long id){
+        try{
+            return ResponseEntity.ok(tabelaNutricionalService.findById(id));
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Optional<TabelaNutricional>> updateTabelaNutricional(@PathVariable Long id, @RequestBody TabelaNutricional tabelaNutricional){
+        try{
+            return ResponseEntity.ok(tabelaNutricionalService.updateTabelaNutricional(tabelaNutricional, id));
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Optional<TabelaNutricional>> deleteTabelaNutricional(@PathVariable Long id){
+        try{
+            tabelaNutricionalService.deleteTabelaNutricional(id);
+            return ResponseEntity.noContent().build();
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
