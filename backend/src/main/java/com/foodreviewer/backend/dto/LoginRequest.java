8a5bde632778f8a9f6dbaa8c2f60281b218985f6
CviPedro <135079780+CviPedro@users.noreply.github.com>
2025-11-11 17:53:44 -0300

    Alteração para se tornar Record e aplicação do DTO pattern

--- backend/src/main/java/com/foodreviewer/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/foodreviewer/backend/dto/LoginRequest.java
@@ -1,33 +1,13 @@
 package com.foodreviewer.backend.dto;
 
-public class LoginRequest {
-    private String email;
-    private String senha;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
-    // Construtor padrão
-    public LoginRequest() {
-    }
+public record LoginRequest(
+        @Email(message = "E-mail inválido")
+        @NotBlank(message = "E-mail é obrigatório")
+        String email,
 
-    // Construtor com todos os campos
-    public LoginRequest(String email, String senha) {
-        this.email = email;
-        this.senha = senha;
-    }
-
-    // Getters e Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-}
+        @NotBlank(message = "Senha é obrigatória")
+        String senha
+) {}
